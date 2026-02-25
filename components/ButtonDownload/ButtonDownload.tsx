8084e013e6c64851b632b7fd80a9b0af74979c3f
kingov.alexey <kingov.alexey@gmail.com>
2026-02-26 01:25:08 +0500

    feat: add structure

---
+++ components/ButtonDownload/ButtonDownload.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ButtonDownload = () => {
+  return (
+    <View>
+      <Text>ButtonDownload</Text>
+    </View>
+  )
+}
+
+export default ButtonDownload
+
+const styles = StyleSheet.create({})
---
+++ components/ResumeCard.tsx/ResumeCard.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ResumeCard = () => {
+  return (
+    <View>
+      <Text>ResumeCard</Text>
+    </View>
+  )
+}
+
+export default ResumeCard
+
+const styles = StyleSheet.create({})
