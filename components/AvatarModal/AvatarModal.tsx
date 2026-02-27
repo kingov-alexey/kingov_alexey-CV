ea267d097911cee21f9ffc37143f632ee40a1a0d
kingov.alexey <kingov.alexey@gmail.com>
2026-02-27 22:42:43 +0500

    feat: add new screens and refactor

---
+++ components/AvatarModal/AvatarModal.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AvatarModal = () => {
+  return (
+    <View>
+      <Text>AvatarModal</Text>
+    </View>
+  )
+}
+
+export default AvatarModal
+
+const styles = StyleSheet.create({})
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, View, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 import ResumeCard from '../components/ResumeCard/ResumeCard';
 import ButtonDownload from '../components/ButtonDownload/ButtonDownload';
 
@@ -8,20 +8,15 @@ const HomeScreen = () => {
     <ScrollView contentContainerStyle={styles.container}>
       <ResumeCard
         fullName="Королев Алексей Викторович"
-        position="Senior Manual QA / Middle AQA / Junior React Native"
+        position="Manual QA / AQA / React Native"
         language="RU / EN / KZ"
       />
-
-      <ButtonDownload title="Скачать Senior Manual RU" pdfUrl="https://github.com/kingov-alexey/kingov_alexey-CV/tree/main/assets/pdf/Korolev_Alexey_CV-QA_RU.pdf" />
-      {/* <ButtonDownload title="Скачать Middle AQA EN" pdfUrl="https://github.com/<username>/cv-app/raw/main/src/assets/pdf/middle_aqa_en.pdf" />
-      <ButtonDownload title="Скачать Junior RN KZ" pdfUrl="https://github.com/<username>/cv-app/raw/main/src/assets/pdf/junior_rn_kz.pdf" />
-      https://github.com/kingov-alexey/kingov_alexey-CV/tree/main/assets/pdf */}
+      <ButtonDownload title="Скачать CV QA/AQA" pdfUrl="https://github.com/kingov-alexey/kingov_alexey-CV/tree/main/assets/pdf/Korolev_Alexey_CV-QA_RU.pdf" />
     </ScrollView>
   )
 }
 
 export default HomeScreen
-
 
 const styles = StyleSheet.create({
   container: { padding: 20 },
